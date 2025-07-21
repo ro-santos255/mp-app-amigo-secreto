dc3fb5c192aea7b5bc50e2ab906b025055aba25a
Romário <romariojdosantos@gmail.com>
2025-07-21 00:48:32 -0300

    feat: Login front

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,9 @@
+import LoginForm from "@/components/login-form";
+
+export default function LoginPage() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen w-full px-4">
+      <LoginForm />
+    </div>
+  );
+} 
