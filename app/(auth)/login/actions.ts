afe117e207bb6acd7713d02f8e9ed3c201ae3a3f
Romário <romariojdosantos@gmail.com>
2025-07-27 01:43:32 -0300

    refactor: Remove unused imports and commented-out signup function in login actions

--- app/(auth)/login/actions.ts
+++ app/(auth)/login/actions.ts
@@ -1,8 +1,5 @@
 'use server'
 
-import { revalidatePath } from 'next/cache'
-import { redirect } from 'next/navigation'
-
 import { createClient } from '@/utils/supabase/server';
 
 export type LoginState = {
@@ -13,8 +10,6 @@ export type LoginState = {
 export async function login(previewState: LoginState, formData: FormData) {
   const supabase = await createClient()
 
-  // type-casting here for convenience
-  // in practice, you should validate your inputs
   const email = formData.get('email') as string
 
   const { error } = await supabase.auth.signInWithOtp({
@@ -36,23 +31,3 @@ export async function login(previewState: LoginState, formData: FormData) {
     message: 'E-mail enviado!'
   }
 }
-
-// export async function signup(formData: FormData) {
-//   const supabase = await createClient()
-
-//   // type-casting here for convenience
-//   // in practice, you should validate your inputs
-//   const data = {
-//     email: formData.get('email') as string,
-//     password: formData.get('password') as string,
-//   }
-
-//   const { error } = await supabase.auth.signUp(data)
-
-//   if (error) {
-//     redirect('/error')
-//   }
-
-//   revalidatePath('/', 'layout')
-//   redirect('/')
-// }
