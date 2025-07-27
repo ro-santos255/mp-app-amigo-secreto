73ad1a0c83eb8421b15a568501d7f988ea6d6b1f
Romário <romariojdosantos@gmail.com>
2025-07-27 01:37:21 -0300

    feat: Update Home component layout with Card structure for improved UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,51 @@
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
 export default function Home() {
   return (
-    <div className="bg-background">Home</div>
+      <main className="container grid mx-auto py-12">
+        <Card className="w-full max-w-3xl mx-auto">
+          <CardHeader>
+            <CardTitle className="text-3xl text-center">
+              Amigo Secreto Digital
+            </CardTitle>
+            <CardDescription className="text-center text-lg">
+              Organize seu amigo secreto de forma simples e divertida
+            </CardDescription>
+          </CardHeader>
+
+          <CardContent className="flex flex-col items-center gap-6">
+            <div className="text-center space-y-0.5">
+              <p className="text-lg">
+                Crie grupos, adicione participantes e realize o sorteio
+                automaticamente
+              </p>
+              <p className="text-muted-foreground">
+                Sem complicações, sem papéis, 100% online
+              </p>
+            </div>
+
+            <div className="flex gap-4">
+              <Link href="/app/grupos/novo">
+                <Button size="lg" variant="default" type="button" className="cursor-pointer">
+                  Criar Novo Grupo
+                </Button>
+              </Link>
+              <Link href="/app/grupos">
+                <Button size="lg" variant="outline" type="button" className="cursor-pointer">
+                  Ver Meus Grupos
+                </Button>
+              </Link>
+            </div>
+          </CardContent>
+        </Card>
+      </main>
   );
 }
